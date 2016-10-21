55b7a5f76f8fc4a1214ac18a20e30057cbc26e2f
Sultanxda <sultanxda@gmail.com>
2016-10-21 01:37:16 -0700

    proc: Remove verifiedbootstate flag from /proc/cmdline
    Userspace parses this and sets the ro.boot.verifiedbootstate prop
    according to the value that this flag has. When ro.boot.verifiedbootstate
    is not 'green', SafetyNet is tripped and fails the CTS test.
    
    Hide verifiedbootstate from /proc/cmdline in order to fix the failed
    SafetyNet CTS check.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,10 +2,13 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -28,6 +31,29 @@ static int __init proc_cmdline_init(void)
 	offset_addr = strstr(saved_command_line, "androidboot.mode=reboot");
 	if (offset_addr != NULL)
 		strncpy(offset_addr + 17, "normal", 6);
+
+	char *offset_addr, *cmd = new_command_line;
+
+	strcpy(cmd, saved_command_line);
+
+	/*
+	 * Remove 'androidboot.verifiedbootstate' flag from command line seen
+	 * by userspace in order to pass SafetyNet CTS check.
+	 */
+	offset_addr = strstr(cmd, "androidboot.verifiedbootstate=");
+	if (offset_addr) {
+		size_t i, len, offset;
+
+		len = strlen(cmd);
+		offset = offset_addr - cmd;
+
+		for (i = 1; i < (len - offset); i++) {
+			if (cmd[offset + i] == ' ')
+				break;
+		}
+
+		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+	}
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
